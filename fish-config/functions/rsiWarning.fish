87fa30739a4d5e2691ce3dcde22829d33a4c79a8
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-09-14 12:42:13 -0300

    feat(fish): grails alias for wrapper, and current time first on the rsi script

--- fish-config/functions/rsiWarning.fish
+++ fish-config/functions/rsiWarning.fish
@@ -1,7 +1,9 @@
 # TODO: find a way to execute this automatically, instead of applying it to
 # a new shell
+# 25 minutes
 set interval 1500
 function rsiWarning --description 'Simple warning to go stretch'
+	echo Script started at (date)!
 	while sleep $interval
 		echo Notify: (date)
 		notify-send "Go stretch!" -u critical
