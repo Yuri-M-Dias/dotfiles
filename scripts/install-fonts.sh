18177a647a6f4b256b69d6d09e02c245f9a78b29
Yuri-M-Dias <yurimathe.yp@gmail.com>
2017-07-06 11:21:09 -0300

    Added OSX check to install fonts

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -5,10 +5,18 @@
 set -e
 sudo -v
 
-echo "Installing Droid Sans Mono for Powerline font"
+echo "Installing Droid Sans Mono for Powerline font with Nerd Icons"
 
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	mkdir -p ~/.local/share/fonts
+	cd ~/.local/share/fonts
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	cd ~/Library/Fonts
+else
+	echo "Couldn't identify the OS! Not doing anything!"
+	exit 1;
+fi
+
 curl -fLo \
 	"Droid Sans Mono for Powerline Nerd Font Complete.otf" \
 	https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
