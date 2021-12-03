eb137d0615b229300ae6c24eafed35c8b9d1a26b
Yuri-M-Dias <yurimathe.yp@gmail.com>
2021-12-03 16:50:57 -0300

    Fixing a small mistake on omf install

--- scripts/install-omf.sh
+++ scripts/install-omf.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-curl -L https://get.oh-my.fish > install_omf
-fish install_omf --path=~/.local/share/omf --config=~/.dotfles/omf
+curl -L https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install_omf
+fish install_omf --path=~/.local/share/omf --config=~/dotfiles/omf
