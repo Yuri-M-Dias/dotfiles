09e0a3f64e6df5f952c658b12e256079374cf682
Yuri-M-Dias <yurimathe.yp@gmail.com>
2022-02-17 14:00:23 +0200

    Adds Pyenv's dependencies to the provisioner

--- installPlugins.sh
+++ installPlugins.sh
@@ -134,6 +134,19 @@ sudo apt install \
     ca-certificates \
     software-properties-common \
     bumblebee \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    llvm \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev \
     mesa-utils \
     -y --verbose-versions
 
